8165222f077f0bde39488ad7e9a3c0e83b49c03f
echoi19 <40866426+echoi19@users.noreply.github.com>
2018-07-18 09:44:11 -0700

    Update fc_model.py

--- fc_model.py
+++ fc_model.py
@@ -48,8 +48,8 @@ img_width, img_height = 150, 150
 
 train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
-nb_train_samples = 800
-nb_validation_samples = 320
+nb_train_samples = 2000
+nb_validation_samples = 800
 epochs = 50
 batch_size = 16
 
