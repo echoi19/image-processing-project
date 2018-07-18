663f38bb24ee0f680c855a0a3b91ea69b11b48a8
echoi19 <40866426+echoi19@users.noreply.github.com>
2018-07-18 09:45:08 -0700

    Update train_vgg16.py

--- train_vgg16.py
+++ train_vgg16.py
@@ -51,7 +51,7 @@ img_width, img_height = 150, 150
 
 train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
-nb_train_samples = 1600
+nb_train_samples = 2000
 nb_validation_samples = 800
 epochs = 2
 batch_size = 16
